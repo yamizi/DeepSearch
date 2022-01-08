c1a8f387da070ad62388ad70ce33267ab203521b
Salah GHAMIZI <salah.ghamizi@uni.lu>
2022-01-08 17:14:11 +0100

    Updated Wrapper to separate the model from the example

--- CIFAR/madryCifarWrapper.py
+++ CIFAR/madryCifarWrapper.py
@@ -17,10 +17,7 @@ from pickle import dump, load
 from sys import argv
 from tensorflow import keras
 import numpy as np
-_, (x_test, y_test) = keras.datasets.cifar10.load_data()
-x_test=x_test/255.0
-x_test=x_test.reshape(10000,32,32,3)
-y_test=y_test.reshape(-1)
+
 class CompatModel:
     def __init__(self,folder):
         self.sess=tf.compat.v1.Session()
@@ -34,4 +31,10 @@ class CompatModel:
         self.calls+=images.shape[0]
         res=np.exp(self.sess.run(self.model.pre_softmax,feed_dict={self.model.x_input:images*255,self.model.y_input:[1]}))
         return res/np.sum(res,axis=1).reshape(-1,1)
-mymodel=CompatModel("model_defended/")
+if __name__ == '__main__':
+    _, (x_test, y_test) = keras.datasets.cifar10.load_data()
+    x_test = x_test / 255.0
+    x_test = x_test.reshape(10000, 32, 32, 3)
+    y_test = y_test.reshape(-1)
+
+    mymodel=CompatModel("model_defended/")
